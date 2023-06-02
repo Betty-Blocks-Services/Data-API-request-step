e1d0f1ed86a0a1f8c8c048f2498def9349cb149d
robinvanderburght <robin.vanderburght@bettyblocks.com>
2023-06-02 11:45:11 +0200

    Pushed V1.0
    Pushed V1.0

---
+++ functions/dataApiRequest/1.0/index.js
@@ -0,0 +1,55 @@
+import templayed from "./templayed";
+
+const dataApiRequest = async ({
+  type,
+  query,
+  filter,
+  filterVariables,
+  model: { name },
+}) => {
+  const variableMap = filterVariables.reduce((previousValue, currentValue) => {
+    previousValue[currentValue.key] = currentValue.value;
+    return previousValue;
+  }, {});
+
+  const queryFilter =
+    filter !== "" && filter !== null
+      ? `(where: {${templayed(filter)(variableMap)}})`
+      : ``;
+
+  const queryTypeName = type === "record" ? `one${name}` : `all${name}`;
+
+  const oneQuery = `
+  query {
+    ${queryTypeName}${queryFilter} {
+    ${query}
+    }
+  }
+`;
+
+  const allQuery = `
+query {
+    ${queryTypeName}${queryFilter} {
+    results {
+        ${query}
+    }
+  }
+}
+`;
+
+  const finalQuery = type === "record" ? oneQuery : allQuery;
+
+  const { data, errors } = await gql(finalQuery);
+
+  if (errors) {
+    console.log(errors);
+  }
+
+  {
+    return {
+      result:
+        type === "record" ? data[queryTypeName] : data[queryTypeName].results,
+    };
+  }
+};
+export default dataApiRequest;
